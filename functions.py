d1f654bd58c0e154719c2314d3ed9ffde6c776ad
Mayuri <gaikwadmayurim@gmail.com>
2022-09-16 14:01:05 +0530

    working on dowload

--- functions.py
+++ functions.py
@@ -21,14 +21,16 @@ def texttoaudio(file_path:str):
         page =pdfReader.pages[0]
         text = page.extractText()
         engine = pyttsx3.init()
+        st.subheader("Voice")
         voices = engine.getProperty('voices')
-        gender=st.radio('Voice', ['Male','Female'])
+        gender=st.radio('Voice', ['Male','Female'],index=1)
         if gender=="Male":
             engine.setProperty('voice', voices[0].id)
-        if gender=="Female":
+        else:
             engine.setProperty('voice', voices[1].id)
         st.subheader('Speed')
-        speed=st.slider("",min_value=0.25,max_value=2.0, step=0.25, value=1.0)    
+        speed=st.slider("",min_value=0.25,max_value=2.0, step=0.25, value=1.0)
+        st.subheader("Rate")
         rate = engine.getProperty('rate') 
         engine.setProperty('rate', rate*speed)
         a=st.number_input("Start index", min_value=0, max_value=pages, value=1, step=1)
@@ -39,10 +41,11 @@ def texttoaudio(file_path:str):
                 text = page.extractText()
                 engine.say(text)
                 engine.runAndWait()
-        if st.button("Download"):
-            st.write("😞 Sorry to disappoint! Working on it!")
+        
+
                 
 def download_audio(file_path:str):
+   
     with open(file_path,'rb') as f:
         pdfReader = PyPDF2.PdfFileReader(f)
         pages = pdfReader.numPages
@@ -64,6 +67,8 @@ if uploaded_file is not None:
        fp.write_bytes(uploaded_file.getvalue())
        show_pdf(tmp_file.name)
        texttoaudio(tmp_file.name)
+       if st.button("Download"):
+           st.write("😞 Sorry to disappoint! Working on it!")
             #download_audio(tmp_file.name)
             #imgs = convert_from_path(tmp_file.name)
             #st.markdown(f"Converted images from PDF")
